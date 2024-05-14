ce9c3132c86de179d3a0e2ac1e948ef04fc0fcd5
vanichx <vanyapetrunin88@gmail.com>
2024-05-14 17:20:21 +0200

    updated branch

--- inc/Webserv.hpp
+++ inc/Webserv.hpp
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Webserv.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kilchenk <kilchenk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ipetruni <ipetruni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:21:20 by ipetruni          #+#    #+#             */
-<<<<<<< .merge_file_fS6R8H
-/*   Updated: 2024/05/10 16:05:29 by kilchenk         ###   ########.fr       */
-=======
-/*   Updated: 2024/05/10 17:46:51 by ipetruni         ###   ########.fr       */
->>>>>>> .merge_file_id48Q6
+/*   Updated: 2024/05/14 17:19:57 by ipetruni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +37,5 @@
 #include "ConfigFileParser.hpp"
 #include "ServerConfiguration.hpp"
 #include "ConfigFile.hpp"
-<<<<<<< .merge_file_fS6R8H
 #include "Client.hpp"
-=======
-#include "Location.hpp"
-#include "WebservUtils.hpp"
-
->>>>>>> .merge_file_id48Q6
 /* Webserv.hpp */
