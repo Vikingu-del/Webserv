47014a983542e30baede54545ff48efee43c50b9
Erik Seferi <eseferi@c1r8s1.42wolfsburg.de>
2024-05-17 11:56:18 +0200

    finished with htpp

--- inc/Http.hpp
+++ inc/Http.hpp
@@ -6,7 +6,7 @@
 /*   By: eseferi <eseferi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 17:44:46 by eseferi           #+#    #+#             */
-/*   Updated: 2024/05/16 20:03:21 by eseferi          ###   ########.fr       */
+/*   Updated: 2024/05/17 11:32:10 by eseferi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,135 +18,135 @@
 
 namespace   HTTP
 {
-    // Constatns
-    const int OK = 200;
-    const int CREATED = 201;
-    const int ACCEPTED = 202;
-    const int NO_CONTENT = 204;
-    const int MOVED_PERMANENTLY = 301;
-    const int FOUND = 302;
-    const int SEE_OTHER = 303;
-    const int NOT_MODIFIED = 304;
-    const int BAD_REQUEST = 400;
-    const int UNAUTHORIZED = 401;
-    const int FORBIDDEN = 403;
-    const int NOT_FOUND = 404;
-    const int METHOD_NOT_ALLOWED = 405;
-    const int REQUEST_TIMEOUT = 408;
-    const int INTERNAL_SERVER_ERROR = 500;
-    const int NOT_IMPLEMENTED = 501;
-    const int BAD_GATEWAY = 502;
-    const int SERVICE_UNAVAILABLE = 503;
-    
-    // ENUMS
-    enum Method
-    {
-        GET,
-        HEAD,
-        POST,
-        PUT,
-        DELETE,
-        TRACE,
-        OPTIONS,
-        CONNECT,
-        PATCH
-    };
+	// ENUMS
+	enum	StatusCode {
+		OK = 200,
+		CREATED = 201,
+		ACCEPTED = 202,
+		NO_CONTENT = 204,
+		MOVED_PERMANENTLY = 301,
+		FOUND = 302,
+		SEE_OTHER = 303,
+		NOT_MODIFIED = 304,
+		BAD_REQUEST = 400,
+		UNAUTHORIZED = 401,
+		FORBIDDEN = 403,
+		NOT_FOUND = 404,
+		METHOD_NOT_ALLOWED = 405,
+		REQUEST_TIMEOUT = 408,
+		INTERNAL_SERVER_ERROR = 500,
+		NOT_IMPLEMENTED = 501,
+		BAD_GATEWAY = 502,
+		SERVICE_UNAVAILABLE = 503
+	};
+	
+	enum	Method
+	{
+		GET,
+		HEAD,
+		POST,
+		PUT,
+		DELETE,
+		TRACE,
+		OPTIONS,
+		CONNECT,
+		PATCH
+	};
 
-    enum Version
-    {
-        HTTP_1_0,
-        HTTP_1_1,
-        HTTP_2_0
-    };
+	enum Version
+	{
+		HTTP_1_0,
+		HTTP_1_1,
+		HTTP_2_0
+	};
 
-    // METHODS
-    std::string methodToString(Method method);
-    Method stringToMethod(const std::string& method);
-    std::string versionToString(Version version);
-    Version stringToVersion(const std::string& version);
+	// METHODS
+	std::string	methodToString(Method method);
+	Method		stringToMethod(const std::string& method);
+	std::string	versionToString(Version version);
+	Version		stringToVersion(const std::string& version);
 
     // CLASSES
-    class   Header
-    {
-        private:
-            std::string _key;
-            std::string _value;
+	class	Header
+	{
+		private:
+			std::string		_key;
+			std::string		_value;
+		public:
+			Header();
+			Header(const std::string &key, const std::string &value);
+			~Header() {};
+			
+			// Setters
+			void			setKey(const std::string &key);
+			void			setValue(const std::string &value);
 
-        public:
-            Header();
-            Header(const std::string &key, const std::string &value);
-            ~Header() {};
-            
-            // Setters
-            void setKey(const std::string &key);
-            void setValue(const std::string &value);
+			// Getters
+			const			std::string &getKey() const;
+			const			std::string &getValue() const;
 
-            // Getters
-            const std::string &getKey() const;
-            const std::string &getValue() const;
+			// Overload
+			bool			operator==(const std::string &key) const;
 
-            // Overload
-            bool operator==(const std::string &key) const;
+			// methods
+			std::string		serialize() const;
+			static Header	deserialize(const std::string &header);
+	};
 
-            // methods
-            std::string serialize() const;
-            static Header deserialize(const std::string &header);
-    };
+	class	Request
+	{
+		private:
+			Method							_method;
+			std::string						_resource;
+			std::map<std::string, Header>	_headers;
+			Version							_version;
+		public:
+			Request();
+			Request(Method method, const std::string& resource, const std::map<std::string, Header>& headers, Version version);
+			~Request() {};
 
-    class   Request
-    {
-        private:
-            Method _method;
-            std::string _resource;
-            std::map<std::string, Header> _headers;
-            Version _version;
-        public:
-            Request();
-            Request(Method method, const std::string& resource, const std::map<std::string, Header>& headers, Version version);
-            ~Request() {};
+		// Setters
+		void								setVersion(Version version);
+		void								setMethod(Method method);
+		void								setResource(const std::string &resource);
+		void									setHeaders(const std::map<std::string, Header> &headers);
 
-        // Setters
-        void setVersion(Version version);
-        void setMethod(Method method);
-        void setResource(const std::string &resource);
-        void setHeaders(const std::map<std::string, Header> &headers);
+		// Getters
+		Version								getVersion() const;
+		Method								getMethod() const;
+		const std::string					&getResource() const;
+		const std::map<std::string, Header>	&getHeaders() const;
 
-        // Getters
-        Version getVersion() const;
-        Method getMethod() const;
-        const std::string &getResource() const;
-        const std::map<std::string, Header> &getHeaders() const;
-
-        // Methods
-        std::string serialize() const;
-        static Request deserialize(const std::string &request);
-    };
+		// Methods
+		std::string							serialize() const;
+		static Request						deserialize(const std::string &request);
+	};
 
     class   Response {
         private:
-            int _responseCode;
-            Version _version;
-            std::map<std::string, Header> _headers;
-            std::string _body;
+            int									_responseCode;
+            Version								_version;
+            std::map<std::string, Header>		_headers;
+            std::string							_body;
         public:
             Response();
             Response(int responseCode, Version version, const std::map<std::string, Header> &headers, const std::string &body);
             ~Response() {};
             
             // Setters
-            void setResponseCode(int responseCode);
-            void setVersion(Version version);
-            void setHeaders(const std::map<std::string, Header> &headers);
-            void setBody(const std::string &body);
+            void								setResponseCode(int responseCode);
+            void								setVersion(Version version);
+            void								setHeaders(const std::map<std::string, Header> &headers);
+            void								setBody(const std::string &body);
 
             // Getters
-            int getResponseCode() const;
-            Version getVersion() const;
-            const std::map<std::string, Header> &getHeaders() const;
-            const std::string &getBody() const;
+            int									getResponseCode() const;
+            Version								getVersion() const;
+            const std::map<std::string, Header>	&getHeaders() const;
+            const std::string					&getBody() const;
             
             // Methods
-            std::string serialize() const;
-            static Response deserialize(const std::string &response);
+            std::string							serialize() const;
+            static Response						deserialize(const std::string &response);
     };
 }
--- src/Http.cpp
+++ src/Http.cpp
@@ -23,72 +23,50 @@
 
 // this function converts the HTTP Method enum to a string
 std::string		HTTP::methodToString(Method method) {
-	static const std::map<Method, std::string> methods = {
-		{GET, "GET"},
-		{HEAD, "HEAD"},
-		{POST, "POST"},
-		{PUT, "PUT"},
-		{DELETE, "DELETE"},
-		{TRACE, "TRACE"},
-		{OPTIONS, "OPTIONS"},
-        {CONNECT, "CONNECT"},
-        {PATCH, "PATCH"}
+	static const std::pair<Method, std::string> methodPairs[] = {
+		std::make_pair(GET, "GET"), std::make_pair(HEAD, "HEAD"), std::make_pair(POST, "POST"), 
+		std::make_pair(PUT, "PUT"), std::make_pair(DELETE, "DELETE"), std::make_pair(TRACE, "TRACE"), 
+		std::make_pair(OPTIONS, "OPTIONS"), std::make_pair(CONNECT, "CONNECT"), std::make_pair(PATCH, "PATCH")
 	};
+	static const std::map<Method, std::string> methods(methodPairs, methodPairs + sizeof(methodPairs) / sizeof(methodPairs[0]));
 	std::map<Method, std::string>::const_iterator it = methods.find(method);
-	if (it != methods.end())
-		return it->second;
-	else
-		return "UNKNOWN";
+	if (it != methods.end()) return it->second;
+	return "UNKNOWN";
 }
 
 // this function converts the HTTP Method string to an enum
 HTTP::Method	HTTP::stringToMethod(const std::string& method) {
-	static const std::map<std::string, Method> methods = {
-        {"GET", GET},
-        {"HEAD", HEAD},
-        {"POST", POST},
-        {"PUT", PUT},
-        {"DELETE", DELETE},
-        {"TRACE", TRACE},
-        {"OPTIONS", OPTIONS},
-        {"CONNECT", CONNECT},
-        {"PATCH", PATCH}
+	static const std::pair<std::string, Method> methodPairs[] = {
+		std::make_pair("GET", GET), std::make_pair("HEAD", HEAD), std::make_pair("POST", POST), std::make_pair("PUT", PUT),
+		std::make_pair("DELETE", DELETE), std::make_pair("TRACE", TRACE), std::make_pair("OPTIONS", OPTIONS),
+		std::make_pair("CONNECT", CONNECT), std::make_pair("PATCH", PATCH)
 	};
+	static const std::map<std::string, Method> methods(methodPairs, methodPairs + sizeof(methodPairs) / sizeof(methodPairs[0]));
 	std::map<std::string, Method>::const_iterator it = methods.find(method);
-	if (it != methods.end())
-		return it->second;
-	else
-		throw std::invalid_argument("Invalid Http Method: " + method);
+	if (it != methods.end()) return it->second;
+	throw std::invalid_argument("Invalid HTTP Method: " + method);
 }
 
 // this function converts the HTTP Version enum to a string
 std::string		HTTP::versionToString(Version version) {
-	static const std::map<Version, std::string> versions = {
-		{HTTP::HTTP_1_0, "HTTP/1.0"},
-		{HTTP::HTTP_1_1, "HTTP/1.1"},
-		{HTTP::HTTP_2_0, "HTTP/2.0"}
+	static const std::pair<Version, std::string> versionPairs[] = {
+		std::make_pair(HTTP_1_0, "HTTP/1.0"), std::make_pair(HTTP_1_1, "HTTP/1.1"), std::make_pair(HTTP_2_0, "HTTP/2.0")
 	};
+	static const std::map<Version, std::string> versions(versionPairs, versionPairs + sizeof(versionPairs) / sizeof(versionPairs[0]));
 	std::map<Version, std::string>::const_iterator it = versions.find(version);
-	if (it != versions.end()) {
-		return it->second;
-	} else {
-		return "UNKNOWN";
-	}
+	if (it != versions.end()) return it->second;
+	return "UNKNOWN";
 }
 
 // this function converts the HTTP Version string to an enum
-HTTP::Version HTTP::stringToVersion(const std::string& version) {
-    static const std::map<std::string, Version> versions = {
-        {"HTTP/1.0", HTTP::HTTP_1_0},
-        {"HTTP/1.1", HTTP::HTTP_1_1},
-        {"HTTP/2.0", HTTP::HTTP_2_0}
-    };
-    auto it = versions.find(version);
-    if (it != versions.end()) {
-        return it->second;
-    } else {
-        throw std::invalid_argument("Invalid Http Version: " + version);
-    }
+HTTP::Version	HTTP::stringToVersion(const std::string& version) {
+	static const std::pair<std::string, Version> versionPairs[] = {
+		std::make_pair("HTTP/1.0", HTTP_1_0), std::make_pair("HTTP/1.1", HTTP_1_1), std::make_pair("HTTP/2.0", HTTP_2_0)
+	};
+	static const std::map<std::string, Version> versions(versionPairs, versionPairs + sizeof(versionPairs) / sizeof(versionPairs[0]));
+	std::map<std::string, Version>::const_iterator it = versions.find(version);
+	if (it != versions.end()) return it->second;
+	throw std::invalid_argument("Invalid HTTP Version: " + version);
 }
 
 
@@ -97,8 +75,8 @@ HTTP::Version HTTP::stringToVersion(const std::string& version) {
 // CLASSES
 /*                HEADER                  */
 
-HTTP::Header::Header() : _key(0), _value(0) {};
-HTTP::Header::Header(const std::string &key, const std::string &value) : _key(key), _value(value) {};
+HTTP::Header::Header() : _key(""), _value("") {}
+HTTP::Header::Header(const std::string &key, const std::string &value) : _key(key), _value(value) {}
 
 // seters
 void	HTTP::Header::setKey(const std::string &key) { this->_key = key; }
@@ -114,21 +92,14 @@ bool	HTTP::Header::operator==(const std::string &key) const { return this->_key 
 
 // methods
 std::string		HTTP::Header::serialize() const {
-	std::string header;
-	header += this->_key;
-	header += ": ";
-	header += this->_value;
-	header += LINE_END;
-	return header;
+	return this->_key + ": " + this->_value + LINE_END;
 }
 
 HTTP::Header	HTTP::Header::deserialize(const std::string &header) {
 	std::vector<std::string> segments = utils::split(header, ": ");
 	if (segments.size() < 2)
 		throw std::runtime_error("Invalid HTTP Header: " + header);
-	const std::string key = segments[0];
-	const std::string value = segments[1];
-	return Header(key, value);
+	return Header(segments[0], segments[1]);
 }
 
 /*                HEADER                  */
@@ -136,8 +107,10 @@ HTTP::Header	HTTP::Header::deserialize(const std::string &header) {
 /*                REQUEST                 */
 
 // Constructors
-HTTP::Request::Request() : _method(HTTP::GET), _resource(0), _headers(), _version(HTTP::HTTP_1_1) {};
-HTTP::Request::Request(Method method, const std::string& resource, const std::map<std::string, Header>& headers, Version version) : _method(method), _resource(resource), _headers(headers), _version(version) {};
+HTTP::Request::Request()
+	: _method(HTTP::GET), _resource(0), _headers(), _version(HTTP::HTTP_1_1) {};
+HTTP::Request::Request(Method method, const std::string& resource, const std::map<std::string, HTTP::Header>& headers, Version version)
+    : _method(method), _resource(resource), _headers(headers), _version(version) {}
 
 // Setters
 void	HTTP::Request::setMethod(Method method) { this->_method = method; }
@@ -146,20 +119,14 @@ void	HTTP::Request::setVersion(Version version) { this->_version = version; }
 void	HTTP::Request::setHeaders(const std::map<std::string, Header> &headers) { this->_headers = headers; }
 
 // Getters
-HTTP::Version HTTP::Request::getVersion() const { return this->_version; }
-HTTP::Method HTTP::Request::getMethod() const { return this->_method; }
-const	std::string &HTTP::Request::getResource() const { return this->_resource; }
-const	std::map<std::string, HTTP::Header> &HTTP::Request::getHeaders() const { return this->_headers; }
+HTTP::Version	HTTP::Request::getVersion() const { return this->_version; }
+HTTP::Method	HTTP::Request::getMethod() const { return this->_method; }
+const			std::string &HTTP::Request::getResource() const { return this->_resource; }
+const			std::map<std::string, HTTP::Header> &HTTP::Request::getHeaders() const { return this->_headers; }
 
 // Methods
 std::string		HTTP::Request::serialize() const {
-	std::string request;
-	request += methodToString(this->_method);
-	request += " ";
-	request += this->_resource;
-	request += " ";
-	request += versionToString(this->_version);
-	request += LINE_END;
+	std::string request = methodToString(_method) + " " + _resource + " " + versionToString(_version) + LINE_END;
 	for (std::map<std::string, HTTP::Header>::const_iterator it = this->_headers.begin(); it != this->_headers.end(); ++it)
     	request += it->second.serialize();
 	request += LINE_END;
@@ -194,9 +161,10 @@ HTTP::Request	HTTP::Request::deserialize(const std::string &request) {
 //            RESPONSE                 */
 
 // Constructors
-HTTP::Response::Response() : _responseCode(0), _version(HTTP::HTTP_1_1), _headers(), _body(0) {};
+HTTP::Response::Response() : _responseCode(0), _version(HTTP::HTTP_1_1), _headers(), _body("") {};
 
-HTTP::Response::Response(int responseCode, Version version, const std::map<std::string, Header> &headers, const std::string &body) : _responseCode(responseCode), _version(version), _headers(headers), _body(body) {};
+HTTP::Response::Response(int responseCode, Version version, const std::map<std::string, Header>& headers, const std::string& body)
+	: _responseCode(responseCode), _version(version), _headers(headers), _body(body) {};
 
 // Setters
 void	HTTP::Response::setResponseCode(int responseCode) { this->_responseCode = responseCode; }
@@ -205,18 +173,14 @@ void	HTTP::Response::setHeaders(const std::map<std::string, Header> &headers) { 
 void	HTTP::Response::setBody(const std::string &body) { this->_body = body; }
 
 // Getters
-int		HTTP::Response::getResponseCode() const { return this->_responseCode; }
-HTTP::Version HTTP::Response::getVersion() const { return this->_version; }
-const std::map<std::string, HTTP::Header>	&HTTP::Response::getHeaders() const { return this->_headers; }
-const std::string &HTTP::Response::getBody() const { return this->_body; }
+int				HTTP::Response::getResponseCode() const { return this->_responseCode; }
+HTTP::Version	HTTP::Response::getVersion() const { return this->_version; }
+const			std::map<std::string, HTTP::Header>	&HTTP::Response::getHeaders() const { return this->_headers; }
+const			std::string &HTTP::Response::getBody() const { return this->_body; }
 
 // Methods
 std::string		HTTP::Response::serialize() const {
-	std::string response;
-	response += versionToString(this->_version);
-	response += " ";
-	response += utils::toString(this->_responseCode);
-	response += LINE_END;
+	std::string response = versionToString(this->_version) + " " + utils::toString(this->_responseCode) + LINE_END;
 	for (std::map<std::string, HTTP::Header>::const_iterator it = this->_headers.begin(); it != this->_headers.end(); ++it)
     	response += it->second.serialize();
 	response += LINE_END;
@@ -239,8 +203,8 @@ HTTP::Response		HTTP::Response::deserialize(const std::string &response) {
 	headerLines.erase(headerLines.begin());
 	std::map<std::string, Header> headers;
 	for (std::vector<std::string>::const_iterator it = headerLines.begin(); it != headerLines.end(); ++it) {
-        const Header header = Header::deserialize(*it);
-        headers.insert(std::make_pair(header.getKey(), header));
-    }
+		const Header header = Header::deserialize(*it);
+		headers.insert(std::make_pair(header.getKey(), header));
+	}
 	return Response(responseCode, version, headers, body);
 }
